20dd8a1c2715f200558957e58eb10a81c63acaa5
akshitverma4 <59332509+akshitverma4@users.noreply.github.com>
2020-04-24 11:56:01 +0530

    Fixed all fragments

--- app/src/main/java/com/example/android/ycet/DashboardActivity.kt
+++ app/src/main/java/com/example/android/ycet/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.ycet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -11,9 +12,9 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.android.ycet.R
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.dashboardactivity.*
 
 class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
 
     private lateinit var toolbar: Toolbar
     private lateinit var drawerLayout: DrawerLayout
@@ -21,10 +22,8 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboardactivity)
-
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
         drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
 
@@ -36,25 +35,41 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         navView.setNavigationItemSelectedListener(this)
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+            R.id.nav_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearlayout, HomeFragment()).commit()
+            }
             R.id.nav_profile -> {
-                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, ProfileView::class.java)
-                startActivity(intent)
+                supportFragmentManager.beginTransaction().replace(R.id.linearlayout, ProfileFragment()).commit()
             }
 
             R.id.nav_documents -> {
-                Toast.makeText(this, "Documents clicked", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, DocumentsView::class.java)
-                startActivity(intent)
+                supportFragmentManager.beginTransaction().replace(R.id.linearlayout, DocumentsFragment()).commit()
             }
 
             R.id.nav_attendance -> {
-                Toast.makeText(this, "Attendance clicked", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, AttendanceView::class.java)
-                startActivity(intent)
+                supportFragmentManager.beginTransaction().replace(R.id.linearlayout, AttendanceFragment()).commit()
             }
 
             R.id.nav_logout -> {
@@ -62,9 +77,7 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
             }
 
             R.id.nav_settings -> {
-                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this,SettingApp::class.java)
-                startActivity(intent)
+                supportFragmentManager.beginTransaction().replace(R.id.linearlayout, SettingsFragment()).commit()
             }
 
         }
